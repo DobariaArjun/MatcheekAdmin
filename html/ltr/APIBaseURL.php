c6801aff1d53528578a8571e1bceddbf353c2e0e
Arjun Dobaria <arjun.dobaria12@gmail.com>
2019-04-25 19:38:44 +0530

    change

--- html/ltr/APIBaseURL.php
+++ html/ltr/APIBaseURL.php
@@ -3,5 +3,5 @@ $BASE_URL_LOCAL = "http://localhost:3000/api/";
 $BASE_URL_HEROKU = "https://switlover.herokuapp.com/api/";
 $BASE_URL_AWS = "http://13.59.195.52:3000/api/";
 
-$BASE_URL = $BASE_URL_HEROKU;
+$BASE_URL = $BASE_URL_AWS;
 ?>
